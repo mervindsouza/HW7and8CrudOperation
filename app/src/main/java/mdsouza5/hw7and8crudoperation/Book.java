53b3c32518346a92cde1fd7e3f9c95b016bbe841
mdsouza5 <mdsouza5@hawk.iit.edu>
2018-04-05 16:46:35 -0500

    Initial Commit

---
+++ app/src/main/java/mdsouza5/hw7and8crudoperation/Book.java
@@ -0,0 +1,9 @@
+package mdsouza5.hw7and8crudoperation;
+
+/**
+ * Created by mdsouza on 4/5/18.
+ */
+
+public class Book {
+    MainActivity ma = new MainActivity();
+}
---
+++ app/src/main/java/mdsouza5/hw7and8crudoperation/MainActivity.java
@@ -0,0 +1,14 @@
+package mdsouza5.hw7and8crudoperation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
