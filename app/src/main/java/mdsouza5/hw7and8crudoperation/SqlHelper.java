141bf1b2347edd42a734fe3a1550c0bb47afd621
mdsouza5 <mdsouza5@hawk.iit.edu>
2018-04-06 09:53:51 -0500

    DDL & DML added in SQLHelper

--- app/src/main/java/mdsouza5/hw7and8crudoperation/SqlHelper.java
+++ app/src/main/java/mdsouza5/hw7and8crudoperation/SqlHelper.java
@@ -3,6 +3,7 @@ package mdsouza5.hw7and8crudoperation;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.ContactsContract;
 
 /**
  * Created by mdsouza on 4/6/18.
@@ -10,19 +11,46 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class SqlHelper extends SQLiteOpenHelper {
 
+    //Assign Database Name
     private static final String DATABASE_NAME = "BOOKDB";
 
-    public SqlHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    //Assign Table Name
+    private static final String TABLE_NAME = "books";
+
+    //Assign Columns for Table - books
+    private static final String COL_BOOK_ID = "id";
+    private static final String COL_BOOK_TITLE = "title";
+    private static final String COL_BOOK_AUTHOR = "author";
+
+    //DML for onUpgrade()
+    private static final String DROP_TABLE = String.format("DROP TABLE IF EXISTS %s", TABLE_NAME);
+
+    //Default Constructor
+    public SqlHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
     }
 
+    //Implemented Methods
+
+    //Configure and enable write ahead logging for atomicity and durability
     @Override
+    public void onConfigure(SQLiteDatabase db) {
+        super.onConfigure(db);
+        db.setForeignKeyConstraintsEnabled(true);
+        db.enableWriteAheadLogging();
+    }
+
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String CREATE_BOOKS_TABLE = String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY, %s TEXT, %s TEXT)",
+                TABLE_NAME, COL_BOOK_ID, COL_BOOK_TITLE, COL_BOOK_AUTHOR);
+        sqLiteDatabase.execSQL(CREATE_BOOKS_TABLE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int previousVersion, int newVersion) {
+        if (previousVersion != newVersion) {
+            sqLiteDatabase.execSQL(DROP_TABLE);
+            onCreate(sqLiteDatabase);
+        }
     }
 }
